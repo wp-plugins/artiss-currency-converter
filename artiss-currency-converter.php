c194bf1a3fe1d55972c2b80f4eb1945c0fcdf093
dartiss <dartiss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-19 10:56:10 +0000

    git-svn-id: https://plugins.svn.wordpress.org/artiss-currency-converter/tags/1.1@574542 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ artiss-currency-converter.php
@@ -0,0 +1,70 @@
+<?php
+/*
+Plugin Name: Artiss Currency Converter
+Plugin URI: http://www.artiss.co.uk/currency-converter
+Description: Convert currency values
+Version: 1.1
+Author: David Artiss
+Author URI: http://www.artiss.co.uk
+*/
+
+/**
+* Artiss Currency Converter
+*
+* Main code - include various functions
+*
+* @package	Artiss-Currency-Converter
+* @since	1.0
+*/
+
+define( 'artiss_currency_converter_version', '1.1' );
+
+/**
+* Plugin initialisation
+*
+* Loads the plugin's translated strings
+*
+* @since	1.1
+*/
+
+function acc_plugin_init() {
+
+    $language_dir = plugin_basename( dirname( __FILE__ ) ) . '/languages/';
+
+	load_plugin_textdomain( 'currency-converter', false, $language_dir );
+
+}
+
+add_action( 'init', 'acc_plugin_init' );
+
+/**
+* Main Includes
+*
+* Include all the plugin's functions
+*
+* @since	1.0
+*/
+
+$functions_dir = WP_PLUGIN_DIR . '/artiss-currency-converter/includes/';
+
+// Include all the various functions
+
+include_once( $functions_dir . 'acc-get-options.php' );             // Fetch/create default options
+
+include_once( $functions_dir . 'acc-shared-functions.php' );		// Shared functionality
+
+include_once( $functions_dir . 'acc-convert-currency.php' );		// Main code to perform currency conversion
+
+if ( is_admin() ) {
+
+    include_once( $functions_dir . 'artiss-plugin-ads.php' );       // Option screen ads
+
+    include_once( $functions_dir . 'acc-admin-config.php' );        // Assorted admin configuration changes
+
+} else {
+
+	include_once( $functions_dir . 'acc-shortcodes.php' );	        // Shortcodes
+
+	include_once( $functions_dir . 'acc-functions.php' );	        // PHP function calls
+}
+?>
